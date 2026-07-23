34f7c04e583705aa8be1a547c37859c536588eee
Pedro Augusto <pedroaugustomoura000@gmail.com>
2026-07-23 17:56:23 -0300

    chores: adiciona depencias, altera versão do projeto e inclui configurações no .properties

---
+++ lojacarros/src/main/java/io/github/pedroaugusto000/lojacarros/LojacarrosApplication.java
@@ -0,0 +1,13 @@
+package io.github.pedroaugusto000.lojacarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojacarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojacarrosApplication.class, args);
+	}
+
+}
